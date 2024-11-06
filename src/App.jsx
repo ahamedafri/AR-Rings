f77bad68608fa1c304b4465a39666813e0925621
ahamedafri <ahamedafricontact@gmail.com>
2024-11-06 15:01:25 +0530

    Add main application component with VirtualRingTryOn integration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VirtualRingTryOn from './components/VirtualRingTryOn';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <VirtualRingTryOn />
+    </div>
+  );
+}
+
+export default App;
